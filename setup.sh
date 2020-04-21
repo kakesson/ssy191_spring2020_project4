42d1ba50d495691a6d5d47063ddefc364cd9f1a6
Knut Åkesson <knut@chalmers.se>
2020-04-21 11:21:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 sudo apt update
 sudo apt install build-essential -y
 sudo apt install gcc-multilib -y
-sudo apt install git -y
-git clone https://github.com/kakesson/ssy191_spring2020_project4.git
+#sudo apt install git -y
+#git clone https://github.com/kakesson/ssy191_spring2020_project4.git
